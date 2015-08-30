8676699dfe4f09f961a66f3a6910452b5c17dd94
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-08-30 15:13:33 +0300

    initial commit

---
+++ comments-by-post-type.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+  Plugin Name: Comments by Post Type
+  Plugin URI: http://github.com/ierhyna/comments-by-post-type/
+  Description: Separate comments by post type in admin menu.
+  Version: 1.0.0
+  Author: Irina Sokolovskaya
+  Author URI: http://oriolo.ru/
+  License: GNU General Public License v2 or later
+  License URI: http://www.gnu.org/licenses/gpl-2.0.html
+  Domain Path: /languages
+
+
+  Copyright 2015  Irina Sokolovskaya  (email : sokolovskaja.irina@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Exclude comments of all post types except "post" from edit-comments.php page.
+ *
+ * @param  array  $clauses
+ * @param  object $wp_comment_query
+ * @return array
+ */
+
+function exclude_cpt_comments($clauses, $wp_comment_query) {
+    global $wpdb;
+    
+    if (!$clauses['join']) {
+        $clauses['join'] = "JOIN $wpdb->posts ON $wpdb->posts.ID = $wpdb->comments.comment_post_ID";
+    }
+    
+    if (!$wp_comment_query->query_vars['post_type']) {
+        
+        $post_types = apply_filters('comment_admin_menu_post_types', get_post_types(array('public' => true, 'show_ui' => true)));
+        
+        foreach ($post_types as $post_type) {
+            if ($post_type != 'post') {
+                $clauses['where'].= $wpdb->prepare(" AND {$wpdb->posts}.post_type != %s", $post_type);
+            }
+        }
+    }
+    
+    return $clauses;
+}
+
+/**
+ * Hook the comments clauses.
+ *
+ * @param object $screen
+ */
+
+function comments_exclude_delay_hook($screen) {
+    if ($screen->id == 'edit-comments') {
+        add_filter('comments_clauses', 'exclude_cpt_comments', 10, 2);
+    }
+}
+
+add_action('current_screen', 'comments_exclude_delay_hook', 10, 2);
+
+/**
+ * Create separated comments menu items for every post type 
+ */
+
+function add_comments_menu_for_ctp() {
+
+    $post_types = apply_filters('comment_admin_menu_post_types', get_post_types(array('public' => true, 'show_ui' => true)));
+
+    foreach ($post_types as $post_type) {
+        if ($post_type != 'post') {
+            
+            add_submenu_page("edit.php?post_type={$post_type}", __('Comments'), __('Comments'), 'moderate_comments', "edit-comments.php?post_type={$post_type}");
+        }
+    }
+}
+
+add_action('admin_menu', 'add_comments_menu_for_ctp');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Plugin Name ===
+Contributors: oriolo
+Donate link: http://example.com/
+Tags: comments, admin menu, custom post types
+Requires at least: 3.0.1
+Tested up to: 4.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Separate comments by post type in admin menu.
+
+== Description ==
+
+The plugin separates comments by post type in admin menu. It moves the comments that are leaved to pages and custom post types to separate pages, located within the post type menu. So you can easily manage the comments to regular posts and custom post types.
+
+== Installation ==
+
+* Download the plugin in `.zip` archive.
+* Upload unzipped `comments-by-post-type` folder to the `/wp-content/plugins/` directory.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial version
